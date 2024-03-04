771d1b508df079340d9867da6c89cd4cd3695bf2
Rob Cheatham <robert.cheatham.uk@gmail.com>
2024-03-04 13:56:59 +0100

    scaled back boilerplate

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+// Required imports
+import Image from "next/image";
+
+// Components, styling, assets
+import styles from "./page.module.css";
+
+export default function HomePage() {
+
+  return(
+    <main>
+      <div>
+        <h1>The Tipsters</h1>
+      </div>
+    </main>
+  );
+
+}
